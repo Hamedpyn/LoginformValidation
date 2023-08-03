2e4f0c5d48cd5d19898bc7fd26c19757d092ff1f
Hamed <hamedpyn@gmail.com>
2023-08-03 23:03:12 +0430

    adding form reset button in js

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,18 +4,22 @@ let
     nameInput = document.querySelector('#name'),
     emailInput = document.querySelector('#email'),
     passWordInput = document.querySelector('#pass'),
-    signUpBtn = document.querySelector('#signUp');
+    signUpBtn = document.querySelector('#signUp'),
+    form = document.querySelector('.form'),
+    resetBtn = document.querySelector('#resetBtn')
 
 function disablingBtn (){
     signUpBtn.disabled = true
-    // signUpBtn.disabled = true
     signUpBtn.setAttribute('style','background-color:#dfdfdfee')
     signUpBtn.style.scale = '1'
 }
 disablingBtn()
 
-nameInput.addEventListener('blur',formValidation)
-emailInput.addEventListener('blur',formValidation)
-passWordInput.addEventListener('blur',formValidation)
+nameInput.addEventListener('blur', formValidation)
+emailInput.addEventListener('blur', formValidation)
+passWordInput.addEventListener('blur', formValidation)
+function formValidation() { }
+resetBtn.addEventListener('click',()=>{
+    form.reset()
+})
 
-function formValidation(){}
